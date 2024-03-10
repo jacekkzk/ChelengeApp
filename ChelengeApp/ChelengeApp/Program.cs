c0aa37fe54df145b120808be35a28586c92cfaa9
1jacekkz <160979536+1jacekkz@users.noreply.github.com>
2024-03-10 17:14:34 -0400

    lekcja 4

--- ChelengeApp/ChelengeApp/Program.cs
+++ ChelengeApp/ChelengeApp/Program.cs
@@ -12,7 +12,7 @@ else if (sex == "Kobieta" && age < 29)
     Console.WriteLine("Ewa, ponizej 29 lat");
 }
 
-else if (sex == "Meszczyzna" && age < 29)
+else if (sex == "Meszczyzna" && age < 18)
 {
     Console.WriteLine("Niepelnoletni Meszczyzna");
 }
